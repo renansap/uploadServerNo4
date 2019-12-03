edf9628a12e354b90c703daf9dbe1f0debca6b9a
Renan Correia <renansap@gmail.com>
2019-12-03 20:34:51 -0200

    Ajustado ServidorNo4

--- ServerNo4/src/serverno4/ServerNo4.java
+++ ServerNo4/src/serverno4/ServerNo4.java
@@ -23,14 +23,14 @@ public class ServerNo4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
-        
-                     ServerSocket serverSocket = null;
+
+        ServerSocket serverSocket = null;
 
         try {
             serverSocket = new ServerSocket(4004);
-            System.out.println("Subiu o servidor 4004");
+            System.out.println("Subiu o ServerNo4");
         } catch (IOException ex) {
-            System.out.println("Can't setup server on this port number. ");
+            System.out.println("Não é possível configurar o servidor neste número da porta. ");
         }
 
         Socket socket = null;
@@ -40,33 +40,33 @@ public class ServerNo4 {
         try {
             socket = serverSocket.accept();
         } catch (IOException ex) {
-            System.out.println("Can't accept client connection. ");
+            System.out.println("Não é possível aceitar a conexão do cliente. ");
         }
 
         try {
             in = socket.getInputStream();
         } catch (IOException ex) {
-            System.out.println("Can't get socket input stream. ");
+            System.out.println("Não é possível obter o fluxo de entrada do soquete. ");
         }
 
         try {
             out = new FileOutputStream("C:\\Users\\Ksa\\Documents\\NetBeansProjects\\uploadServerNo1\\src\\arquivos\\novo4.txt");
+            System.out.println("Arquivo Copiando SeverNo4");
+
+            byte[] bytes = new byte[16 * 1024];
+
+            int count;
+            while ((count = in.read(bytes)) > 0) {
+                out.write(bytes, 0, count);
+            }
+
+            out.close();
+            in.close();
+            socket.close();
+            serverSocket.close();
+            // TODO code application logic here
         } catch (FileNotFoundException ex) {
-            System.out.println("File not found. ");
+            System.out.println("Arquivo não encontrado. ");
         }
-
-        byte[] bytes = new byte[16 * 1024];
-
-        int count;
-        while ((count = in.read(bytes)) > 0) {
-            out.write(bytes, 0, count);
-        }
-
-        out.close();
-        in.close();
-        socket.close();
-        serverSocket.close();
-        // TODO code application logic here
     }
-    
 }
