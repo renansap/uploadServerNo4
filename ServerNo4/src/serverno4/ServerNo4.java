b3eee84262bc715b19e31425de687d1d621a3192
Igor Viana do Amaral <igornoia89@gmail.com>
2019-12-04 19:07:54 -0200

    Merge branch 'master' of https://github.com/renansap/uploadJava
    # Conflicts:
    #	Upload/src/uploads/TelaUp.java

--- ServerNo4/src/serverno4/ServerNo4.java
+++ ServerNo4/src/serverno4/ServerNo4.java
@@ -5,6 +5,12 @@
  */
 package serverno4;
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -26,7 +32,8 @@ public class ServerNo4 {
 
         ServerSocket serverSocket = null;
         
-        String caminho = "C:\\Users\\Ksa\\Documents\\NetBeansProjects\\uploadServerNo4\\ServerNo4\\src\\arquivos\\";
+        //String caminho = "C:\\Users\\Ksa\\Documents\\NetBeansProjects\\uploadServerNo4\\ServerNo4\\src\\arquivos\\";
+        String caminho = "C:\\Users\\igor.amaral\\Documents\\NetBeansProjects\\arquivos\\";
 
         try {
             serverSocket = new ServerSocket(4004);
@@ -52,23 +59,63 @@ public class ServerNo4 {
         }
 
         try {
-            out = new FileOutputStream(caminho+"novo4.txt");
-            System.out.println("Arquivo Copiando SeverNo4");
+            int bytesAmount = 0;
+            byte[] bytes = new byte[1024 * 1024];
+            //BufferedReader InFromClient = new BufferedReader (new InputStreamReader(socket.getInputStream()));
+            //DataOutputStream OutToClient = new DataOutputStream(socket.getOutputStream());
+            
+            final DataInputStream InFromClient = new DataInputStream(socket.getInputStream());
+            final DataOutputStream OutToClient = new DataOutputStream(socket.getOutputStream());            
+            int a = InFromClient.read();
+            if (a==0) {
+                System.out.println(InFromClient.read());
+                File env = new File(caminho + "novo.004");
+                System.out.println(env.getPath());
+                
+                FileInputStream fis = new FileInputStream(env);
+                BufferedInputStream bis = new BufferedInputStream(fis);
+                
+                OutputStream out1 = socket.getOutputStream();
+                while ((bytesAmount = bis.read(bytes)) > 0) {
+                    out1.write(bytes, 0, bytesAmount);
+                    //OutToClient.write(bytes, 0, bytesAmount);
+                }
+                System.out.println("envio do arquivo 004 para o cliente");
+            } else {
+                out = new FileOutputStream(caminho + "novo.004");
+                           //System.out.println("out "+ out.getClass().getName());
 
-            byte[] bytes = new byte[16 * 1024];
+                String caminhoServer = (caminho + "novo.004");
 
-            int count;
-            while ((count = in.read(bytes)) > 0) {
-                out.write(bytes, 0, count);
+                File busca = new File(caminhoServer);
+
+                FileInputStream fis1 = new FileInputStream(busca);
+                BufferedInputStream bis1 = new BufferedInputStream(fis1);
+
+               //        try (   FileInputStream fis = new FileInputStream(busca);
+                //                BufferedInputStream bis = new BufferedInputStream(fis)) {
+                //            
+                //        }
+                //        byte[] arq1 = busca;;
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+                //byte[] bytes = new byte[16 * 1024];
+
+                int count;
+                while ((count = in.read(bytes)) > 0) {
+                    out.write(bytes, 0, count);
+                }
+
+                out.close();
+                in.close();
+                socket.close();
+                serverSocket.close();               
             }
 
-            out.close();
-            in.close();
-            socket.close();
-            serverSocket.close();
-            // TODO code application logic here
-        } catch (FileNotFoundException ex) {
-            System.out.println("Arquivo não encontrado. ");
+
+            
+        } catch (IOException ex) {
+            System.out.println(ex);
         }
     }
 }
